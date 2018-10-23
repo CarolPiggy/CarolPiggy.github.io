dde632bfd4faf27a181fb606c171294ce1dcefd6
ZillyRex <zillyrain@gmail.com>
2018-10-23 15:17:01 +0800

    new

---
+++ index.js
@@ -0,0 +1,23 @@
+var now = new Date();
+function GetServerTime()
+{
+    var urodz = new Date("12/22/2018 08:30:00");//设定倒计时的时间
+    now.setTime(now.getTime()+250);
+    days = (urodz - now) / 1000 / 60 / 60 / 24;
+    daysRound = Math.floor(days);
+    hours = (urodz - now) / 1000 / 60 / 60 - (24 * daysRound);
+    hoursRound = Math.floor(hours);
+    minutes = (urodz - now) / 1000 /60 - (24 * 60 * daysRound) - (60 * hoursRound);
+    minutesRound = Math.floor(minutes);
+    seconds = (urodz - now) / 1000 - (24 * 60 * 60 * daysRound) - (60 * 60 * hoursRound) - (60 * minutesRound);
+    secondsRound = Math.round(seconds);
+    var obj = document.getElementById("time_second");
+    daysRound = daysRound;
+    if(daysRound >= 0) {
+        obj.innerHTML="<h1><span>"+daysRound+"</span> 天 <span>"+hoursRound+"</span> 时 <span>"+minutesRound+"</span> 分 <span>"+secondsRound+"</span> 秒</h1>" ;
+    }
+}
+setInterval("GetServerTime()",500);
+
+GetServerTime()
+
